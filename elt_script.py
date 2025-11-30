8b4d058417677a9e2e44f61714c2bda03bc5700d
maximekgn <kogonmaxime@gmail.com>
2025-11-30 15:40:03 +0000

    Update docker-compose configuration and replace PostgreSQL images to version 15 due to compatibilities; remove unused scripts and initialize database with new schema

--- elt_script.py
+++ elt_script.py
@@ -1,29 +1,23 @@
 import subprocess
 import time
 
-# vérifie si la BD postgres en en service
-def wait_for_postgres(host,max_retries=5, delay_seconds=5):
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
     retries = 0
     while retries < max_retries:
         try:
             result = subprocess.run(
-                ["pg_isready", "-h", host],check=True, capture_output=True, text=True
+                ["pg_isready", "-h", host], check=True, capture_output=True, text=True
             )
             if "accepting connections" in result.stdout:
                 print(f"PostgreSQL is ready on host {host}.")
                 return True
         except subprocess.CalledProcessError as e:
             print(f"Error details: {e}")
-            retries+=1
+            retries += 1
             print(f"PostgreSQL is not ready on host {host}. Attempt {retries}/{max_retries}. Retrying in {delay_seconds} seconds...")
             time.sleep(delay_seconds)
     print(f"PostgreSQL is not ready on host {host} after {max_retries} attempts.")
     return False
-
-if not wait_for_postgres("source_postres"):
-    exit(1)
-
-print("Starting ELT process...")
 
 source_config = {
     'dbname': 'source_db',
@@ -31,15 +25,25 @@ source_config = {
     'password': 'source_password',
     'host': 'source_postgres',
 }
+
 destination_config = {
     'dbname': 'destination_db',
     'user': 'destination_user',
     'password': 'destination_password',
     'host': 'destination_postgres',
 }
+print("Waiting for source PostgreSQL...")
+if not wait_for_postgres(source_config['host']):
+    exit(1)
+
+print("Waiting for destination PostgreSQL...")
+if not wait_for_postgres(destination_config['host']):
+    exit(1)
+
+print("Starting ELT process...")
 
 # Commande pg_dump pour exporter la base de données source
-dump_commande = [
+dump_command = [
     'pg_dump',
     '-h', source_config['host'],
     '-U', source_config['user'],
@@ -49,14 +53,15 @@ dump_commande = [
 ]
 
 subprocess_env = dict(PGPASSWORD=source_config['password'])
-subprocess.run(dump_commande, env=subprocess_env, check=True)
+subprocess.run(dump_command, env=subprocess_env, check=True)
 
+# Commande pour charger les données dans la destination
 load_command = [
     'psql',
     '-h', destination_config['host'],
-    '-u', destination_config['user'],
+    '-U', destination_config['user'],
     '-d', destination_config['dbname'],
-    '-a','-f' , 'data_dump.sql',
+    '-a', '-f', 'data_dump.sql',
 ]
 
 subprocess_env = dict(PGPASSWORD=destination_config['password'])
